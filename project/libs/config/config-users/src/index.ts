dbb36b49e6cb9cc6bb4de7178515e4519276bb2d
Ivan Davydov <imdavydov@outlook.com>
2023-03-31 12:54:12 +0300

    Завершает подключение mongoose к users

--- project/libs/config/config-users/src/index.ts
+++ project/libs/config/config-users/src/index.ts
@@ -1,3 +1,4 @@
-export * from './lib/db.config';
 export * from './lib/config-users.module';
-export * from './lib/app.config';
+export { default as dbConfig } from './lib/config/db.config';
+export { default as appConfig } from './lib/config/app.config';
+export * from './lib/get-mongoose-options';
