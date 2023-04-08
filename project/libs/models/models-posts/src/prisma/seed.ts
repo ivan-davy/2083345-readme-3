302893a2cc5b4066019e3ef35c4b98b7cc9d91e3
Ivan Davydov <imdavydov@outlook.com>
2023-04-08 16:17:06 +0300

    WIP - исправления. Сейчас не работает сидинг БД

--- project/libs/models/models-posts/src/prisma/seed.ts
+++ project/libs/models/models-posts/src/prisma/seed.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from '@prisma/client';
+import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
-
 async function fillDb() {
   await prisma.post.upsert({
     where: { postId: 1 },
