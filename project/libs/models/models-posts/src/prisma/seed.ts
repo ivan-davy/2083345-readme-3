82b30deba578e458e0d38d91b0ba595bfaaf083f
Ivan Davydov <imdavydov@outlook.com>
2023-04-08 14:10:24 +0300

    WIP - добавляет schema для prisma. Работоспособность еще не проверялась

---
+++ project/libs/models/models-posts/src/prisma/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function fillDb() {
+  await prisma.post.upsert({
+    where: { postId: 1 },
+    update: {},
+    create: {
+      likesQty: 5,
+      type: 'text',
+      status: 'posted',
+      tags: ['wow'],
+      title: 'TITLE',
+      announcement: 'ANNOUNCE',
+      text: 'NEWTEXTTTT',
+      comments: {
+        create: [
+          {
+            authorId: 'userr',
+            text: 'commentepic'
+          },
+        ]
+      },
+    }
+  });
+  console.info('🤘️ Database was filled')
+}
+
+fillDb()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect()
+
+    process.exit(1);
+  })
