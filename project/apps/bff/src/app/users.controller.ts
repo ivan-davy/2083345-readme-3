46551bda356df8c62f51b801154cb2276c85c125
ivan-davy <imdavydov@outlook.com>
2023-04-30 19:06:08 +0300

    Добавляет users-контроллер BFF

--- project/apps/bff/src/app/users.controller.ts
+++ project/apps/bff/src/app/users.controller.ts
@@ -1,8 +1,9 @@
-import {Body, Controller, Post, Req, UseFilters} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Req, UseFilters} from '@nestjs/common';
 import {HttpService} from '@nestjs/axios';
 import {LoginUserDto} from './dto/login-user.dto';
 import {ApplicationServiceURL} from './app.config';
 import {AxiosExceptionFilter} from './filters/axios-exception.filter';
+import {CreateUserDto} from './dto/create-user.dto';
 
 @Controller('users')
 @UseFilters(AxiosExceptionFilter)
@@ -14,6 +15,26 @@ export class UsersController {
   @Post('login')
   public async login(@Body() loginUserDto: LoginUserDto) {
     const { data } = await this.httpService.axiosRef.post(`${ApplicationServiceURL.Auth}/login`, loginUserDto);
+    return data;
+  }
+
+  @Post('register')
+  public async register(@Body() createUserDto: CreateUserDto) {
+    const { data } = await this.httpService.axiosRef.post(`${ApplicationServiceURL.Auth}/register`, createUserDto);
+    return data;
+  }
+
+  @Get(':id')
+  public async getUser(
+    @Req() req: Request,
+    @Param('id') id: string,
+  ) {
+    const { data } = await this.httpService.axiosRef.get(`${ApplicationServiceURL.Auth}/${id}`, {
+      headers: {
+        'Authorization': req.headers['authorization']
+      }
+    });
+
     return data;
   }
 
